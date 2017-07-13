faf6d5605e558c01aba1eaad3b31cd66e0ad9c1b
Brennen Bull <brennenbull@gmail.com>
2017-07-12 20:28:04 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(window).scroll(function(){
+  let dist = $(this).scrollTop();
+
+  if(dist > $('.project-gal').offset().top - ($(window).height() / 1.2)) {
+
+    $('.filler').each(function(i){
+
+      setTimeout(function(){
+        $('.filler').eq(i).addClass('is-showing');
+      }, (700 * (Math.exp(i * 0.14))) - 700);
+
+    });
+  }
+});
