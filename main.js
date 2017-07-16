6d955310c990369a3886d6bf0b1cbb410a24a437
Brennen Bull <brennenbull@gmail.com>
2017-07-16 13:00:02 -0600

    add css sketching

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+$(document).ready(function(){
+  loadDots();
+})
 
+
+function loadDots(){
+  for (var i = 0; i < 1500; i++) {
+    $('.dot-1').clone().addClass('dot').attr('id',`${i}`).removeClass('dot-1').appendTo('.dots');
+  }
+  $('#282').removeClass('dot').addClass('white-dot')
+  $('#336').removeClass('dot').addClass('white-dot')
+  $('#390').removeClass('dot').addClass('white-dot')
+}
 
 $(window).scroll(function(){
   let dist = $(this).scrollTop();
-  console.log(dist);
+
+  console.log($(window).width());
   if(dist <= 60){
     $('.filler').removeClass('is-showing');
   }
@@ -26,11 +39,16 @@ $(window).scroll(function(){
     $('.skills-img').css('transform', `translate(${Math.max(0, (x - 500))}px, 0px)`);
   }
 
-  if(dist > $('.footer').offset().top - ($(window).height() / 1.2)){
+  if(dist > $('.footer').offset().top - ($(window).height() / 1.2) && $(window).width() > 1000){
     let x = Math.floor($('.footer').offset().top) - dist;
     $('.back-img').css('transform', `translate(${Math.min(0, (-x + 500))}px, 0px )`);
 
     $('.background').css('transform', `translate(${Math.max(0, (x - 500))}px, 0px)`);
   }
 
+});
+
+$('.carousel').carousel({
+  interval: false,
+  ride: true
 });
